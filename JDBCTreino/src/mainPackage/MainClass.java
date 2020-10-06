12501ca47354efd16319987e4c373853988f52c2
Alabarse <Mateusalabarse@hotmail.com>
2020-10-05 23:49:46 -0300

    First commit

---
+++ JDBCTreino/src/mainPackage/MainClass.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+import viewPackage.JPrincipal;
+
+public class MainClass {
+
+  
+    public static void main(String[] args) {
+       JPrincipal jp = new JPrincipal();
+       jp.setVisible(true);
+       jp.setLocation(800, 300);
+    }
+    
+}
