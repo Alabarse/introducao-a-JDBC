e5f1fd712b12718718ce288914a0ff2c09f7f6ab
Alabarse <Mateusalabarse@hotmail.com>
2020-10-06 00:23:15 -0300

    Iniciando projeto

---
+++ SistemaEscolar/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.JLogin;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JLogin jl = new JLogin(null, true);
+        jl.setVisible(true);
+        
+    }
+}
